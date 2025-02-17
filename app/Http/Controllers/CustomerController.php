1a784cb9b089a50c49fe1512b9f2d0c0e43d0e26
B00153274 Grace Moore <B00153274@mytudublin.ie>
2025-02-17 16:09:21 +0000

    fourth commit

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -20,5 +20,19 @@ class CustomerController extends Controller
         $customer->save();
     }
 
+    public function edit($id)
+    {
+         $customer = Customer::find($id);
+         return view('customers.edit')->with('customer', $customer);
+    }
+
+    public function update(Request $request)
+    {
+        $customer = Customer::find($request->id);
+        $customer->setFirstname($request->firstname);
+        $customer->setSurname($request->surname);
+        $customer->save();
+    }
+
 }
 ?>
